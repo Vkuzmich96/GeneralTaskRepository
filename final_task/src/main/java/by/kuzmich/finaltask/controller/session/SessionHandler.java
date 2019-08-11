421345f74a7ddd1c735aecdf562cd0875c998395
Vlad <vladislav.kuzmich.96@mail.ru>
2019-08-11 15:53:01 +0300

    Убрал ненужные методы из класса обработчика сессии

--- final_task/src/main/java/by/kuzmich/finaltask/controller/session/SessionHandler.java
+++ final_task/src/main/java/by/kuzmich/finaltask/controller/session/SessionHandler.java
@@ -8,12 +8,6 @@ public interface SessionHandler {
     void create(HttpServletRequest req, User user);
     String getRole(HttpServletRequest req);
     String getLogin(HttpServletRequest req);
-    int getGraphId (HttpServletRequest req);
     boolean isExists(HttpServletRequest req);
-    void setGraphId (HttpServletRequest req, int id);
-    void setActionId(HttpServletRequest req, int childId);
-    void incrementStep(HttpServletRequest req);
-    void changeActualActionId(HttpServletRequest req);
     void close (HttpServletRequest req);
-    void setActualActionId(HttpServletRequest req, int actualId);
 }
--- final_task/src/main/java/by/kuzmich/finaltask/controller/session/SessionHandlerUser.java
+++ final_task/src/main/java/by/kuzmich/finaltask/controller/session/SessionHandlerUser.java
@@ -1,6 +1,5 @@
 package by.kuzmich.finaltask.controller.session;
 
-import by.kuzmich.finaltask.bean.Role;
 import by.kuzmich.finaltask.bean.User;
 import by.kuzmich.finaltask.KeyWordsList;
 
@@ -12,16 +11,7 @@ public class SessionHandlerUser implements SessionHandler {
     public void create(HttpServletRequest req, User user){
             HttpSession session = req.getSession(false);
             session.setAttribute(KeyWordsList.LOGIN, user.getEmail());
-
-            Role userRole = user.getRole();
             session.setAttribute(KeyWordsList.ROLE, user.getRole());
-
-            if (Role.LAWER == userRole){
-                session.setAttribute(KeyWordsList.GRAPH_ID, null);
-                session.setAttribute(KeyWordsList.STEP, KeyWordsList.FIRST_STEP);
-                session.setAttribute(KeyWordsList.ACTION_ID, null);
-                session.setAttribute(KeyWordsList.ACTUAL_ACTION_ID, null);
-            }
     }
 
     public String getRole(HttpServletRequest req){
@@ -32,35 +22,8 @@ public class SessionHandlerUser implements SessionHandler {
         return (String) req.getSession().getAttribute(KeyWordsList.LOGIN);
     }
 
-    public int getGraphId (HttpServletRequest req) {
-        return (Integer) req.getSession().getAttribute(KeyWordsList.GRAPH_ID);
-    }
-
     public boolean isExists(HttpServletRequest req){
         return req.getSession(false).getAttributeNames().hasMoreElements();
-    }
-
-    public void setGraphId (HttpServletRequest req, int id){
-        req.getSession().setAttribute(KeyWordsList.GRAPH_ID, id);
-    }
-
-    public void setActionId(HttpServletRequest req, int childId) {
-        req.getSession().setAttribute(KeyWordsList.ACTION_ID, childId);
-    }
-
-    public void setActualActionId(HttpServletRequest req, int actualId) {
-        req.getSession().setAttribute(KeyWordsList.ACTUAL_ACTION_ID, actualId);
-    }
-
-    public void changeActualActionId(HttpServletRequest req) {
-        Integer id = (Integer) req.getSession().getAttribute(KeyWordsList.ACTION_ID);
-        req.getSession().setAttribute(KeyWordsList.ACTUAL_ACTION_ID, id);
-    }
-
-    public void incrementStep(HttpServletRequest req){
-        Integer step = (Integer) req.getSession().getAttribute(KeyWordsList.STEP);
-        step = ++step;
-        req.getSession().setAttribute(KeyWordsList.STEP, step);
     }
 
     public void close (HttpServletRequest req) {
